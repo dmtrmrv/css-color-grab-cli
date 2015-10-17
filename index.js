77f1aa3f5753ea96bfd1b85d0ffaf389a23842ab
Dmitry Mayorov <iamdmitrymayorov@gmail.com>
2015-10-17 15:27:24 +0300

    Output is now sorted. Fixed issue with border colors. Removed All done! message for simple output.

--- index.js
+++ index.js
@@ -13,26 +13,26 @@ var inputFile  = userArgs[1];
 var outputFile = userArgs[2];
 
 // List of properties that define color.
-var properties = [
-	'color',
-	'background',
-	'background-color',
-	'background-image',
-	'border',
-	'border-top',
-	'border-right',
-	'border-bottom',
-	'border-left',
-	'border-color',
-	'border-top-color',
-	'border-right-color',
-	'border-bottom-color',
-	'border-left-color',
-	'outline',
-	'outline-color',
-	'text-shadow',
-	'box-shadow'
-];
+var properties = {
+	'color': {},
+	'background': {},
+	'background-color': {},
+	'background-image': {},
+	'border': {},
+	'border-top': {},
+	'border-right': {},
+	'border-bottom': {},
+	'border-left': {},
+	'border-color': {},
+	'border-top-color': {},
+	'border-right-color': {},
+	'border-bottom-color': {},
+	'border-left-color': {},
+	'outline': {},
+	'outline-color': {},
+	'text-shadow': {},
+	'box-shadow': {},
+};
 
 /**
  * Checks if property allows color.
@@ -40,7 +40,7 @@ var properties = [
  * @return {bool}
  */
 function propAllowColor( prop ) {
-	return properties.indexOf( prop ) > -1;
+	return properties.hasOwnProperty( prop );
 }
 
 /**
@@ -65,32 +65,117 @@ function processInput( data ) {
 	// Define empty.
 	var outputCss = '';
 
-	// Object containing the list of color properties and selectors.
-	var selectors = {};
-
 	/*
 	 * Walk through each declaration, check if property allow color
 	 * and if the color is the one we are looking for.
-	 * If yes, add a property and selector to the 'selectors' object.
+	 * If yes, add a property and selector to the 'properties' object.
 	 * If property is already present in the object, just add the selector.
 	 */
 	postcss.parse( css ).walkDecls( function ( decl ) {
 		if ( propAllowColor( decl.prop ) && valueHasColor( decl.value, color ) ) {
-			if ( ! selectors.hasOwnProperty( decl.prop ) ) {
-				selectors[ decl.prop ] = {};
-				selectors[ decl.prop ].selectors = [];
-				selectors[ decl.prop ].value = '';
+
+			if ( decl.prop == 'border' ) {
+
+				if ( ! properties[ 'border-color' ].hasOwnProperty( 'selectors' ) ) {
+					properties[ 'border-color' ].selectors = [];
+				}
+
+				if ( ! properties[ 'border-color' ].hasOwnProperty( 'value' ) ) {
+					properties[ 'border-color' ].value = '';
+				}
+
+				properties[ 'border-color' ].selectors.push( decl.parent.selector );
+				properties[ 'border-color' ].value = '#' + color;
+
+			} else if ( decl.prop == 'border-top' ) {
+
+				if ( ! properties[ 'border-top-color' ].hasOwnProperty( 'selectors' ) ) {
+					properties[ 'border-top-color' ].selectors = [];
+				}
+
+				if ( ! properties[ 'border-top-color' ].hasOwnProperty( 'value' ) ) {
+					properties[ 'border-top-color' ].value = '';
+				}
+
+				properties[ 'border-top-color' ].selectors.push( decl.parent.selector );
+				properties[ 'border-top-color' ].value = '#' + color;
+
+			} else if ( decl.prop == 'border-right' ) {
+
+				if ( ! properties[ 'border-right-color' ].hasOwnProperty( 'selectors' ) ) {
+					properties[ 'border-right-color' ].selectors = [];
+				}
+
+				if ( ! properties[ 'border-right-color' ].hasOwnProperty( 'value' ) ) {
+					properties[ 'border-right-color' ].value = '';
+				}
+
+				properties[ 'border-right-color' ].selectors.push( decl.parent.selector );
+				properties[ 'border-right-color' ].value = '#' + color;
+
+			} else if ( decl.prop == 'border-bottom' ) {
+
+				if ( ! properties[ 'border-bottom-color' ].hasOwnProperty( 'selectors' ) ) {
+					properties[ 'border-bottom-color' ].selectors = [];
+				}
+
+				if ( ! properties[ 'border-bottom-color' ].hasOwnProperty( 'value' ) ) {
+					properties[ 'border-bottom-color' ].value = '';
+				}
+
+				properties[ 'border-bottom-color' ].selectors.push( decl.parent.selector );
+				properties[ 'border-bottom-color' ].value = '#' + color;
+
+			} else if ( decl.prop == 'border-left' ) {
+
+				if ( ! properties[ 'border-left-color' ].hasOwnProperty( 'selectors' ) ) {
+					properties[ 'border-left-color' ].selectors = [];
+				}
+
+				if ( ! properties[ 'border-left-color' ].hasOwnProperty( 'value' ) ) {
+					properties[ 'border-left-color' ].value = '';
+				}
+
+				properties[ 'border-left-color' ].selectors.push( decl.parent.selector );
+				properties[ 'border-left-color' ].value = '#' + color;
+
+			} else if ( decl.prop == 'outline' ) {
+
+				if ( ! properties[ 'outline-color' ].hasOwnProperty( 'selectors' ) ) {
+					properties[ 'outline-color' ].selectors = [];
+				}
+
+				if ( ! properties[ 'outline-color' ].hasOwnProperty( 'value' ) ) {
+					properties[ 'outline-color' ].value = '';
+				}
+
+				properties[ 'outline-color' ].selectors.push( decl.parent.selector );
+				properties[ 'outline-color' ].value = '#' + color;
+
+			} else {
+
+				if ( ! properties[ decl.prop ].hasOwnProperty( 'selectors' ) ) {
+					properties[ decl.prop ].selectors = [];
+				}
+
+				if ( ! properties[ decl.prop ].hasOwnProperty( 'value' ) ) {
+					properties[ decl.prop ].value = '';
+				}
+
+				properties[ decl.prop ].selectors.push( decl.parent.selector );
+				properties[ decl.prop ].value = decl.value;
 			}
-			selectors[ decl.prop ].selectors.push( decl.parent.selector );
-			selectors[ decl.prop ].value = decl.value;
+
 		}
+
 	} );
 
 	// Build the CSS.
-	for ( var k in selectors ) {
-		if ( selectors.hasOwnProperty( k ) ) {
-			var prop = selectors[k];
-			outputCss += prop.selectors.join() + ' { ' + k + ': ' + prop.value + ' }\n\n';
+	for ( var k in properties ) {
+		if ( properties.hasOwnProperty( k ) ) {
+			if ( properties[k].hasOwnProperty( 'selectors' ) && properties[k].hasOwnProperty( 'value' ) ) {
+				outputCss += properties[k].selectors.join() + ' { ' + k + ': ' + properties[k].value + ' }\n\n';
+			}
 		}
 	}
 
@@ -102,7 +187,6 @@ function processInput( data ) {
 			console.log( 'All done!' );
 		} else {
 			console.log( outputCss )
-			console.log( 'All done!' );
 		}
 	}
 }
